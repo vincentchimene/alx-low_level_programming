77d006d84db1c637d34010adab074d4e61573b62
Vincent Opurum <vincentchimene@gmail.com>
2022-09-08 19:44:35 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)/* Program to print using puts function */
+{
+		/* String initialization */
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+
+	puts(Mystr); /*prints Mystr with a new line */
+	return (0);
+}
