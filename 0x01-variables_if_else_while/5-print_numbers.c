a91b674df73a9be4c09dc010f628159065942111
Vincent Opurum <vincentchimene@gmail.com>
2022-09-09 13:33:03 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%i", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
