8517248b4a6c2bee54ab0cf17118a8c07ddcbde1
Vincent Opurum <vincentchimene@gmail.com>
2022-09-09 11:00:38 -0700

    Task 2 initial commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* A program that prints the alphabet in lowercase, followed by a new line.*/
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a', c <= 'z', c++)
+		putchar(c);
+		putchar('\n');
+	return (0);
+}
