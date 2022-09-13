0916f8f69744e108f14a5ee2b191c95664904cb1
Vincent Opurum <vincentchimene@gmail.com>
2022-09-13 16:23:29 -0700

    inittial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: A program that prints _putchar followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
