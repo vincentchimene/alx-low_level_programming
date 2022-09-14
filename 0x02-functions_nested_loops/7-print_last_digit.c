99b8110d6944bdaee5c3b461218c481cb5335576
Vincent Opurum <vincentchimene@gmail.com>
2022-09-14 15:08:04 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a num
+ * return: the last digit
+ * @n: int data type
+ * Return: c
+ */
+int print_last_digit(int n)
+{
+	int c = n % 10;
+
+	return (c);
