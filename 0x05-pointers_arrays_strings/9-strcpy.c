49e2bb9cd637e8fd57d45cde65a58739ebe787f2
Vincent Opurum <vincentchimene@gmail.com>
2022-09-20 21:57:49 -0700

    add

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - copies the string pointed to by src
+ * @dest: destination
+ * @src: source
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
