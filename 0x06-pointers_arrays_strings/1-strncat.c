148c038fb58153bac5939b769baf761e72d74a27
Vincent Opurum <vincentchimene@gmail.com>
2022-09-22 08:46:10 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: destination (string1)
+ * @src: source(string to add)
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		continue;
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
