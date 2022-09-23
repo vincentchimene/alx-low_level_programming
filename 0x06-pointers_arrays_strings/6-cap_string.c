a701279209868ade23d0c702ebd385860298438a
Vincent Opurum <vincentchimene@gmail.com>
2022-09-22 17:10:29 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * cap_string - a function that capitalizes all words of a string
+ * @str: input string
+ * Return: caps on first letter of a separator
+ */
+char *cap_string(char *str)
+{
+	int i, x;
+	int cap = 32;
+	int sp[] = {',', ';', '.', '?', '"',
+		 '(', ')', '{', '}', ' ', '\n', '\t'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - cap;
+		}
+
+		cap = 0;
+
+		for (x = 0; x <= 12; x++)
+		{
+			if (str[i] == sp[x])
+			{
+				x = 12;
+				cap = 32;
+			}
+		}
+	}
+	return (str);
+}
