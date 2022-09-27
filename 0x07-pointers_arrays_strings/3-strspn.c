96222e86f0d1bc343a2a74c212da53e9c33e6b72
Vincent Opurum <vincentchimene@gmail.com>
2022-09-26 21:58:01 -0700

    try again

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,17 +8,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, count;
+	unsigned int i, j, count, flag;
 
 	count = 0;
 
 	for (i = 0; accept[i] != '\0'; i++)
 	{
+		flag = 0;
 		for (j = 0; s[j] != '\0'; j++)
 		{
 			if (accept[i] == s[j])
 				count++;
+				flag = 1;
 		}
+		if (flag == 0)
+			return (count);
 	}
 	return (count);
 }
