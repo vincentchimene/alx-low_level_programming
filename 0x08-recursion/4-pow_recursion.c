750dcca9e42b495aa7724e88989312afcdb19c68
Vincent Opurum <vincentchimene@gmail.com>
2022-09-28 18:23:59 -0700

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - Evaluates x raised to the power of y
  * @x: input one
  * @y: input two
- * Return: -1 for y < 0, 0 if y == 0 
+ * Return: -1 for y < 0, 0 if y == 0
  */
 int _pow_recursion(int x, int y)
 {
