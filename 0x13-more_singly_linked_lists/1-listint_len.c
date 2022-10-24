29725bea40ef39ab4eda73b020b20968eedc13c6
Vincent Opurum <vincentchimene@gmail.com>
2022-10-24 16:07:23 -0700

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: a node of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
