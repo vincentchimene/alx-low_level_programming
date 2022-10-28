cdeaf50c845e7a9cfc44b40875fae0ec32936d03
Vincent Opurum <vincentchimene@gmail.com>
2022-10-27 21:51:54 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,33 +1,28 @@
 #include "main.h"
+
+/**
+ * _right_pbin_mask - recursion function to print bit 0 (acts as a mask)
+ * @n: type const unsigned long int
+ */
+
+void _right_pbin_mask(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	_right_pbin_mask(n >> 1);
+	_putchar((n & 1) + '0');
+}
+
 /**
  * print_binary - prints the binary representation of a number
  * @n: type const unsigned long int
  * Return: binary number
  */
+
 void print_binary(unsigned long int n)
 {
-	int i = 0, c, m, temp;
-
 	if (n == 0)
-	{
-		printf("0");
-		return;
-	}
-
-	temp = n;
-
-	while (temp != 0)
-	{
-		i++;
-		temp = temp >> 1;
-	}
-
-	for (c = i - 1; c >= 0; c--)
-	{
-		m = n >> c;
-		if (m & 1)
-			printf("1");
-		else
-			printf("0");
-	}
+		_putchar('0');
+	else
+		_right_pbin_mask(n);
 }
