ad0b60daf815f76fe6dd003c09d8b1e9eb9ff37e
Vincent Opurum <vincentchimene@gmail.com>
2023-05-22 20:37:44 -0700

    created the dynamic librabry, libdynamic.so

---
+++ 0x18-dynamic_libraries/cfiles/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string to count its length
+ * Return: the string lenth
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
